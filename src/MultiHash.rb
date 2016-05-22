a9970e7b000ec35e160b4edbabe3da658dc84209
Rob Reid <memberships1@gmail.com>
2016-05-21 20:24:08 -0700

    updating iteration

--- src/MultiHash.rb
+++ src/MultiHash.rb
@@ -5,32 +5,58 @@ class MultiHash
   
   def initialize(levels, default)
     @levels = levels
-    if @levels < 1
+    if @levels < 2
       raise Exception.new("levels must be higher than 1")
     end
-    if @levels == 1
-      @hash = Hash.new { OneLevelHash.new(default) }
+    puts "dev: init MH #{@levels}"
+    if @levels == 2
+      @hash = Hash.new { |h,k| h[k] = OneLevelHash.new(default) }
     else
-      @hash = Hash.new { MultiHash.new(@levels-1,default) }
+      @hash = Hash.new { |h,k| h[k] = MultiHash.new(@levels-1,default) }
     end
   end
   
   def get(*keys)
-    return @hash[keys[0]].get(keys[1..-1])
+    puts "in get #{@levels}"
+    if keys.length != @levels
+      raise ArgumentError.new("incorrect number of keys (#{keys.length}) supplied, #{@levels} required")
+    end
+    return @hash[keys[0]].get(*keys[1..-1])
   end
   
   # TODO: how do we update a value, for instance, increment a count?
   def put(*args)
-    @hash[args[0]].put(args[1..-1])
+    puts "dev: MH#{@levels}.put(#{args})"
+    if args.length != @levels + 1
+      raise ArgumentError.new("incorrect number of arguments (#{args.length}) supplied, #{@levels + 1} required")
+    end
+    # puts "dev: adding key #{args[0]}"
+    @hash[args[0]].put(*args[1..-1])
+    # puts "dev: after put #{@levels} keys = #{@hash.keys()}"
   end
   
   def remove(keys)
     # remove any entries matching the keys
+    # TODO: fill this in
   end
   
   def each
     puts "in MultiHash.each level #{@levels}"
     return @hash.each { |k,v| yield([k,v]) }
+  end
+
+  def get_all
+    # puts "in get_all #{@levels}"
+    # puts "we have keys of #{@hash.keys()}"
+    resp = []
+    @hash.each do |k,v|
+      # puts "dev: process k=#{k}"
+      for e in v.get_all
+        array = [k].push(*e)
+        resp << array
+      end
+    end
+    return resp
   end
   
 end
@@ -40,15 +66,19 @@ class OneLevelHash
   @hash = nil
   
   def initialize(default)
+    # puts "dev: init OLH"
     @hash = Hash.new {default}
   end
   
   def get(key)
+    # puts "dev OLH.get(#{key})"
     return @hash[key]
   end
   
-  def put(args)
+  def put(*args)
+    # puts "dev: OLH put[#{args[0]}] #{args[1]}"
     @hash[args[0]] = args[1]
+    # puts "dev: after put OLH keys = #{@hash.keys()}"
   end
   
   def remove(key)
@@ -56,7 +86,18 @@ class OneLevelHash
   end
   
   def each
-    puts "in OneLevelHash.each"
+    # puts "in OneLevelHash.each"
     return @hash.each { |k,v| yield([k,v]) }
   end
+
+  def get_all
+    # puts "OLH get_all"
+    # puts "we have keys of #{@hash.keys()}"
+    resp = []
+    @hash.each do |k,v|
+      resp << [k,v]
+    end
+    return resp
+  end
+  
 end 
--- src/TestMultiHash.rb
+++ src/TestMultiHash.rb
@@ -14,26 +14,45 @@ class TestMultiHash
 end
 
 
-hash = OneLevelHash.new(0)
+#hash = OneLevelHash.new(0)
+#
+#puts hash.get(0)
+#puts hash.get(1)
+#
+#hash.put([1,"one"])
+#puts hash.get(1)
+#
+#puts hash
+#
+#hash.each do |k,v|
+#  puts k.to_s + " " + v
+#end
 
-puts hash.get(0)
-puts hash.get(1)
-
-hash.put([1,"one"])
-puts hash.get(1)
-
-puts hash
-
-hash.each do |k,v|
-  puts k.to_s + " " + v
-end
-
-hash = MultiHash.new(3,0)
+puts "work with MH 2"
+hash = MultiHash.new(2,0)
+hash.put("one","one-one",11)
 hash.put("one","one-two",12)
+hash.put("two","two-one",21)
 
-hash.each do |k,v|
-  puts k.to_s + " " + v
+puts "Now try get_all"
+for e in hash.get_all
+  puts e.join(",")
 end
+
+
+puts "work with MH 3"
+hash = MultiHash.new(3,0)
+hash.put("one","one-one","one-one-one",111)
+hash.put("one","one-two","one-two-one",121)
+hash.put("two","two-one","two-one-one",211)
+
+puts "Now try get_all"
+for e in hash.get_all
+  puts e.join(",")
+end
+
+puts hash.get("two","two-one","two-one-two")
+puts hash.get("two","two-two")
 
 
 puts "done with all tests"
